a65615d395bad956d454e45493c004658b8b1545
guide-point <53050583+guide-point@users.noreply.github.com>
2026-02-28 10:00:14 +0530

    Update telegram.py

--- services/telegram.py
+++ services/telegram.py
@@ -1,1 +1,26 @@
+import requests
+import time
+#endpoint to get the chat-id https://api.telegram.org/bot8688967737:AAEM7yp0xt_Go7xo4wC9iowo7KFEenVDfRA/getUpdates
+
+token = "8688967737:AAEM7yp0xt_Go7xo4wC9iowo7KFEenVDfRA"
+
+chat_ids = [
+    1902518342,
+    556677889,
+    998877665,
+]
+
+message = "Warning: Flood expected in your area within the next 24 hours. Please take necessary precautions and stay safe."
+
+for chat_id in chat_ids:
+    response = requests.post(
+        f"https://api.telegram.org/bot{token}/sendMessage",
+        data={
+            "chat_id": chat_id,
+            "text": message,
+        },
+    )
+    print(response.json())
+    time.sleep(0.05)
+
 
